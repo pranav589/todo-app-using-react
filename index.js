d3ee1432e779e4989cc7a14c71e7a510a6aa862e
pranav589 <pranav.bakale@gmail.com>
2020-08-02 12:16:54 +0530

    2 files modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import Hello from './App';
 import './style.css';
 
 class App extends Component {
